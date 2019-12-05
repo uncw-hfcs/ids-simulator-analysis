fb26ff28ea18fcf133dd998ff34dfc826e011070
Lucas Layman <laymanl@uncw.edu>
2019-12-04 21:22:37 -0500

    Added confusion matrix and performance metrics per user

--- event_stats.py
+++ event_stats.py
@@ -16,18 +16,9 @@ def calc_difficulty(event, event_decisions):
     event['group3_count'] = len(event_decisions[(event_decisions.event_id == event['id']) & (event_decisions.user.str.endswith('3'))])
     event['group3_correct'] = len(event_decisions[(event_decisions.event_id == event['id']) & (event_decisions.user.str.endswith('3')) & (event_decisions.escalate == event.should_escalate)])         
 
-
-    if event['group1_count'] == 0:
-        event['group1_difficulty'] = np.NaN
-    else:
-        event['group1_difficulty'] = event['group1_correct'] / event['group1_count']
-
-    if event['group3_count'] == 0:
-        event['group3_difficulty'] = np.NaN
-    else:
-        event['group3_difficulty'] = event['group3_correct'] / event['group3_count']
-
-    event['total_difficulty'] = (event['group1_difficulty']+event['group3_difficulty']) / 2
+    event['group1_difficulty'] = np.NaN if event['group1_count'] == 0 else event['group1_correct'] / event['group1_count']
+    event['group3_difficulty'] = np.NaN if event['group3_count'] == 0 else event['group3_correct'] / event['group3_count']
+    event['total_difficulty'] = np.nansum([event['group1_correct'], event['group3_correct']]) / np.nansum([event['group1_count'], event['group3_count']])    
 
     print(f"{event['id']}: Group1: {event['group1_correct']}/{event['group1_count']} ({event['group1_difficulty']*100:2.0f}), " \
           f"Group 3: {event['group3_correct']}/{event['group3_count']} ({event['group3_difficulty']*100:2.0f}), " \
@@ -36,10 +27,24 @@ def calc_difficulty(event, event_decisions):
     return event
 
 
+def calc_confusion(user, events, event_decisions): 
+    user_decisions = event_decisions[event_decisions.user == user.user]    
+    for decision in user_decisions.itertuples():
+        answer = events[events.id == decision.event_id].should_escalate.item()
+        if answer == 'Escalate':
+            user[decision.event_id] = 'TP' if decision.escalate == 'Escalate' else 'FN'
+        else:
+            user[decision.event_id] = 'FP' if decision.escalate == 'Escalate' else 'TN'            
+    return user    
+
 file = Path('backups') / 'cry-wolf_20191021_13-51-49_MIS310.xlsx'
 
 events = pd.read_excel(file, sheet_name='Event')
 event_decisions = pd.read_excel(file, sheet_name='EventDecision')
+
+# Need to drop "check" events
+events = events[(events['id'] != 74) & (events['id'] != 75)]
+event_decisions = event_decisions[(event_decisions.event_id != 74) & (event_decisions.event_id != 75)]
 
 # Keep only most recent decision per event per user
 event_decisions.sort_values('time_event_decision', inplace=True)
@@ -53,8 +58,23 @@ events['should_escalate'] = events.apply(normalize_answer, axis=1)
 # Calculate correctness measures per group and append onto dataframe
 events = events.apply(calc_difficulty, axis=1, args=(event_decisions,))
 
+# Compute confusion matrix for each user
+users = pd.DataFrame(event_decisions.user.unique(), columns=['user'])
+event_ids = sorted(list(event_decisions.event_id.unique()))
+users = users.reindex(columns = ['user'] + event_ids)
+users = users.apply(calc_confusion, axis=1, args=(events, event_decisions))
+users['TP'] = (users[event_ids] == 'TP').sum(axis=1)
+users['FP'] = (users[event_ids] == 'FP').sum(axis=1)
+users['FN'] = (users[event_ids] == 'FN').sum(axis=1)
+users['TN'] = (users[event_ids] == 'TN').sum(axis=1)
 
-print(events.head())
+# Compute performance measures per user
+users['sensitivity'] = users['TP'] / (users['TP'] + users['FN'])
+users['specificity'] = users['TN'] / (users['TN'] + users['FP'])
+users['precision'] = users['TP'] / (users['TP'] + users['FP'])
 
+
+
+print(users.head())
 
 
