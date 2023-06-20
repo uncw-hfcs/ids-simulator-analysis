a4f6036a519a7e038f5520c61c3f0ad4311886c1
Lucas Layman <laymanl@uncw.edu>
2023-06-20 09:59:02 -0400

    Fixed duplicate_decision parsing bug and added comments

--- duplicate_decision.py
+++ duplicate_decision.py
@@ -1,24 +1,32 @@
 from pathlib import Path
+
 import openpyxl
 
+
 class EventDecision:
-    def __init__(self, row):
-        self.time = row[0].value
-        self.decision = row[1].value
-        self.user = row[2].value
-        self.confidence = row[3].value
-        self.event_id = row[4].value
-        self.decision_id = row[5].value
+    def __init__(self, _row):
+        self.time = _row[1].value
+        self.decision = _row[2].value
+        self.user = _row[3].value
+        self.confidence = _row[4].value
+        self.event_id = _row[5].value
+        self.decision_id = _row[0].value
 
     def __repr__(self):
         return f'({self.user},{self.event_id},{self.decision},{self.confidence})'
-    
+
     def __str__(self):
         return self.__repr__()
 
     def __eq__(self, other):
-        return self.user == other.user and self.event_id == other.event_id and self.decision == other.decision and self.confidence == other.confidence
-    
+        """
+        Equality in an EventDecisions is when the user, event_id, decision, and confidence are equal.
+        :param other: the EventDecision to compare to
+        :return: True or False
+        """
+        return self.user == other.user and self.event_id == other.event_id \
+            and self.decision == other.decision and self.confidence == other.confidence
+
     def __hash__(self):
         return hash((self.user, self.event_id, self.decision, self.confidence))
 
@@ -30,25 +38,29 @@ file = Path('backups') / 'cry-wolf_20200125_14-35-09_patched.xlsx'
 wb = openpyxl.load_workbook(file)
 event_sheet = wb['EventDecision']
 
-users = {}
-resubmit_count = 0
-row_count = 0
+# users is a dictionary whose key is the username. the Values are a dictionary of eventId : Set(EventDecision).
+users: dict[str: dict[str:set[EventDecision]]] = {}
+resubmit_count = 0  # this will count the total number of resubmissions, including resubmissions that do not change
+event_decision_count = 0
 for row in event_sheet.iter_rows(min_row=2):
-    row_count += 1
+    event_decision_count += 1
     ed = EventDecision(row)
+
     if ed.user not in users:
-        users[ed.user] = { ed.event_id : {ed} }
+        users[ed.user] = {ed.event_id: {ed}}
     else:
         if ed.event_id in users[ed.user]:
+            # The value here is added to a Set. Thus, equivalent EventDecisions will not be saved.
             users[ed.user][ed.event_id].add(ed)
+            # This will capture all resubmissions
             resubmit_count += 1
         else:
             users[ed.user][ed.event_id] = {ed}
 
-print(f"Number of unique event decisions: {row_count}")
+print(f"Number of unique event decisions: {event_decision_count}")
 print(f"Number of resubmitted event decisions: {resubmit_count}")
 print(f"Number of unique users: {len(users.keys())}")
-print("Users+event ids with changes on resubmit:")        
+print("Users+event ids with changes on resubmit:")
 
 count_changed = 0
 count_changed_events = 0
@@ -62,12 +74,10 @@ for user, v in users.items():
                 change_counts_by_user[user] += 1
             else:
                 change_counts_by_user[user] = 1
-            count_changed += len(decisions)-1
+            count_changed += len(decisions) - 1
             count_changed_events += 1
             print(sorted(decisions))
 
 print(f"Number of changes on resubmit: {count_changed}")
 print(f"Number of unique events that were changed: {count_changed_events}")
 print(f"Users who changed their answers: {change_counts_by_user}")
-
-
